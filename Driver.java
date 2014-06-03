970bc7279fb99dfb5c7c5a3704cf9d07e34e1982
Shehtab Zaman <shehtabzaman@Shehtabs-MacBook-Air.local>
2014-06-03 09:55:26 -0400

    driver

---
+++ Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+	public static void main(String[] args) {
+		Profile x = new Profile(); 
+		System.out.println(x.name); 
+		System.out.println(x.age); 
+		System.out.println(x.password);
+	}
+}
--- Profile.java
+++ Profile.java
@@ -2,20 +2,35 @@ import java.util.*;
 
 public class Profile{
 
-	public ArrayList<Profile> friendslist; 
+	//public ArrayList<Profile> friendslist; 
 	public String name; 
 	public int age; 
 	public String password; 
-	public String status; 
-	public String birthday; 
-	public Arraylist<String> interests; 
+	//public String status; 
+	//public String birthday; 
+	//public Arraylist<String> interests; 
 	
 	public Profile(){
- 		
+ 		ProfileMaker();
 	}
 
-	public void ProfileMaker(){
-		Scanner st = new Scanner(); 
-		  
+	public void ProfileMaker(){ 
+		System.out.println("please enter your name");
+		Scanner entry = new Scanner(System.in);
+        String name = entry.nextLine(); 
+		
+		System.out.println("please enter your age");
+		Scanner entry2 = new Scanner(System.in);
+        int age = entry2.nextInt();
+
+        System.out.println("please enter your password");
+		Scanner entry3 = new Scanner(System.in);
+        String password = entry.nextLine();
+
+       this.name = name;
+       this.password = password;  
+       this.age = age; 
 	}
+
+
 }
