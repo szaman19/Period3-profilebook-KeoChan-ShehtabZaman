6d27a0ce16af3aa79f1475405cfb2cafab89f03c
Shehtab Zaman <shehtabzaman@Shehtabs-MacBook-Air.local>
2014-06-03 09:45:02 -0400

    first upload

---
+++ Profile.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Profile{
+
+	public ArrayList<Profile> friendslist; 
+	public String name; 
+	public int age; 
+	public String password; 
+	public String status; 
+	public String birthday; 
+	public Arraylist<String> interests; 
+	
+	public Profile(){
+ 		
+	}
+
+	public void ProfileMaker(){
+		Scanner st = new Scanner(); 
+		  
+	}
+}
