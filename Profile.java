c84ba9f7395ff9a546b94c545a639f484a2bc548
Shehtab Zaman <shehtabzaman@Shehtabs-MacBook-Air.local>
2014-06-12 09:39:23 -0400

    update

--- Profile.java
+++ Profile.java
@@ -2,19 +2,8 @@ import java.util.*;
 
 public class Profile{
 
-<<<<<<< HEAD
-	public ArrayList<Profile> friendslist; 
-	public String name; 
-	public int age; 
-	public String password; 
-	public ArrayList<status> status; 
-	public String birthday; 
-	//public Arraylist<String> interests; 
-	
-	public Profile(){
- 		//test for if profile already exists
- 		ProfileMaker();
-=======
+
+
     public ArrayList<Profile> friendslist; 
     protected String name; 
     protected int age; 
@@ -40,8 +29,7 @@ public class Profile{
 	    return age;
 	}else{
 	    return -1;
->>>>>>> 31188990169df8ef26b4b74f5d1ef3cac822f1f2
-	}
+
     }
 
     public void print(){
@@ -94,21 +82,6 @@ public class Profile{
 
     public boolean logIn(){
 
-	/*
-	System.out.println("Username:");
-	Scanner sc = new Scanner(System.in);
-	String un = sc.nextLine();
-	*/
-
-<<<<<<< HEAD
-        System.out.println("When is your birthday?")
-        Scanner entry4 = new Scanner(System.in); 
-        String birthday = entry4.nextLine(); 
-       this.name = name;
-       this.password = password;  
-       this.age = age; 
-       this.birthday = birthday; 
-=======
 	System.out.println("Password:");
 	Scanner sc2 = new Scanner(System.in);
 	String pw = sc2.nextLine();
@@ -118,7 +91,7 @@ public class Profile{
 	}else{
 	    System.out.println("Log in failed.");
 	    return false;
->>>>>>> 31188990169df8ef26b4b74f5d1ef3cac822f1f2
+
 	}
     }
 
